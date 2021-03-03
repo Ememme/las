a315ce8a8b8a61bac6d85486b04ded63dda68756
Ememme <magda.mieczynska@gmail.com>
2021-03-03 18:24:43 +0100

    Add howler to play winning sound

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import "./style.css";
+import { Howl } from "howler";
+
+const sound = new Howl({
+  src: [
+    "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/openhat.wav"
+  ]
+});
 
 const images = {
   treasure: "💰",
@@ -9,6 +16,7 @@ const trees = document.querySelectorAll(".tree").forEach(item => {
   item.addEventListener("mouseover", event => {
     if (item.classList.contains("treasure")) {
       item.innerHTML = images.treasure;
+      sound.play();
       alert("Znalazłeś SKARB!!!!");
     } else {
       item.innerHTML = images.leafs;
